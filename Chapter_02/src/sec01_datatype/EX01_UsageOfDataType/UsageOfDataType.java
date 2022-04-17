13411f70ff1c0fe773833c9d748e6cbb02b850ee
yunjichoi9151 <cyjcode99@gmail.com>
2022-04-18 02:43:53 +0900

    Add chapter 2 datatype files

---
+++ Chapter_02/src/sec01_datatype/EX01_UsageOfDataType/UsageOfDataType.java
@@ -0,0 +1,13 @@
+package sec01_datatype.EX01_UsageOfDataType;
+
+public class UsageOfDataType {
+	public static void main(String[] args) {
+		// 변수 선언과 함께 값 대입
+		int a = 3;
+		// 변수 선언과 값 대입 분리
+		int b;
+		b = 4;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
---
+++ Chapter_02/src/sec01_datatype/EX03_RangeOfVariableUse/RangeOfVariableUse.java
@@ -0,0 +1,15 @@
+package sec01_datatype.EX03_RangeOfVariableUse;
+
+public class RangeOfVariableUse {
+	public static void main(String[] args) {
+		int value1 = 3;
+		{
+			int value2 = 5;
+			System.out.println(value1);
+			System.out.println(value2);
+		}
+		
+		System.out.println(value1);
+		// System.out.println(value2);    // 오류
+	}
+}
