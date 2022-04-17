1cb4a7f3d9381898676301b4493aab5a66941c69
yunjichoi9151 <cyjcode99@gmail.com>
2022-04-17 19:20:11 +0900

    Add chapter 1 basicsyntax files

---
+++ Chapter_01/src/sec01_basicsyntax/EX01_JavaBasicStructure/JavaBasicStructure.java
@@ -0,0 +1,12 @@
+package sec01_basicsyntax.EX01_JavaBasicStructure;
+/* Ctrl+Shift+'/'
+   처음 만든 클래스
+   (여러 줄 주석)
+ */
+
+public class JavaBasicStructure {
+	public static void main(String[] args) {
+		// 1줄 주석 : 화면 출력 코드 Ctrl+'/'
+		System.out.println("콘솔 화면에 출력");
+	}
+}
---
+++ Chapter_01/src/sec01_basicsyntax/EX02_ByteCodeFiles/ByteCodeFiles.java
@@ -0,0 +1,14 @@
+package sec01_basicsyntax.EX02_ByteCodeFiles;
+
+class A {     // A.class
+}
+class B {     // B.class
+}
+class C {     // C.class
+	class D {     // C$D.class  
+	}
+}
+public class ByteCodeFiles {     // ByteCodeFiles.class
+	public static void main(String[] args) {
+	}
+}
