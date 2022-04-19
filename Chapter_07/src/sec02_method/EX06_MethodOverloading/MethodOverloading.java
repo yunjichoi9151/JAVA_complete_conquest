7157adf4035f0a207078714d75b924db386ed6ed
yunjichoi9151 <cyjcode99@gmail.com>
2022-04-20 03:56:05 +0900

    Add chapter 7 method files

---
+++ Chapter_07/src/sec02_method/EX06_MethodOverloading/MethodOverloading.java
@@ -0,0 +1,33 @@
+package sec02_method.EX06_MethodOverloading;
+
+public class MethodOverloading {
+	public static void main(String[] args) {		
+		print();
+		print(3);
+		print(5.8);
+		print(2, 5);
+	}	
+	public static void print() {
+		System.out.println("데이터가 없습니다.");
+	}	
+	public static void print(int a) {
+		System.out.println(a);
+	}	
+	public static void print(double a) {
+		System.out.println(a);
+	}
+	/* 
+	public static void print(double b) {
+		System.out.println(b);
+	}
+	*/	
+	public static void print(int a, int b) {
+		System.out.println("a: "+ a + " b: "+b);
+	}
+	/*
+	public static int print(int a, int b) {
+		System.out.println("a: "+ a + " b: "+b);
+		return a+b;
+	} 
+	*/
+}
