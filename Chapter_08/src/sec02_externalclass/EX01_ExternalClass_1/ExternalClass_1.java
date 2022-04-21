a371abfb7b9ce9f437bb65e3f2736512538b6860
yunjichoi9151 <cyjcode99@gmail.com>
2022-04-22 01:33:48 +0900

    Add chapter 8 externalclass files

---
+++ Chapter_08/src/sec02_externalclass/EX01_ExternalClass_1/ExternalClass_1.java
@@ -0,0 +1,16 @@
+package sec02_externalclass.EX01_ExternalClass_1;
+
+class A{
+	int m=3;
+	int n=4;	
+	void print() {
+		System.out.println(m+", "+n);
+	}
+}
+public class ExternalClass_1 {
+	public static void main(String[] args) {
+		A a = new A();
+		a.print();
+	}
+}
+
---
+++ Chapter_08/src/sec02_externalclass/EX02_ExternalClass_2/pack2/ExternalClass_2.java
@@ -0,0 +1,10 @@
+package sec02_externalclass.EX02_ExternalClass_2.pack2;
+import sec02_externalclass.EX02_ExternalClass_2.pack1.A;
+
+// 외부 클래스를 별도의 파일로 분리
+public class ExternalClass_2 {
+	public static void main(String[] args) {
+		A a = new A();
+		a.print();
+	}
+}
