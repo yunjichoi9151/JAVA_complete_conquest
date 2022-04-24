1e38069cf397d351675b1b4beb4f835004a83364
yunjichoi9151 <cyjcode99@gmail.com>
2022-04-25 04:13:33 +0900

    Add chapter 13 innerinterface files

---
+++ Chapter_13/src/sec05_innerinterface/EX01_CreateObjectOfInnerInterface/CreateObjectOfInnerInterface.java
@@ -0,0 +1,28 @@
+package sec05_innerinterface.EX01_CreateObjectOfInnerInterface;
+
+class A {
+	interface B {
+		public abstract void bcd();
+	}	
+}
+class C implements A.B {
+	public void bcd() {
+		System.out.println("이너 인터페이스 구현 클래스 생성");
+	}
+}
+public class CreateObjectOfInnerInterface {
+	public static void main(String[] args) {
+		// 객체생성 방법 1(자식클래스 직접 생성)
+		A.B ab = new C();
+		C c = new C();		
+		c.bcd();
+		
+		// 객체생성 방법 2(익명이너클래스 생성)		
+		A.B b = new A.B() {			
+			public void bcd() {
+				System.out.println("익명 이너 클래스로 객체 생성");
+			}
+		};	
+		b.bcd();		
+	}
+}
