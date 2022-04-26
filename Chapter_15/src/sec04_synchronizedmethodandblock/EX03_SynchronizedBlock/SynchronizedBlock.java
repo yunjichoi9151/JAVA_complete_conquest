30d4465086807b4c07000024c16faee7e0d1f18f
yunjichoi9151 <cyjcode99@gmail.com>
2022-04-27 00:25:34 +0900

    Add chapter 15 synchronizedmethodandblock files

---
+++ Chapter_15/src/sec04_synchronizedmethodandblock/EX03_SynchronizedBlock/SynchronizedBlock.java
@@ -0,0 +1,47 @@
+package sec04_synchronizedmethodandblock.EX03_SynchronizedBlock;
+
+// 공유객체
+class MyData {
+	int data = 3;
+	
+	public void plusData() {
+		synchronized (this) {
+			int mydata = data; // 데이터 가져오기
+			try { Thread.sleep(2000);} catch (InterruptedException e) {}		
+			data = mydata+1;
+		}
+	}
+}
+
+// 공유 객체를 사용하는 쓰레드 
+class PlusThread extends Thread {
+	MyData myData;
+	public PlusThread(MyData myData) {
+		this.myData=myData;
+	}
+	
+	@Override
+	public void run() {
+		myData.plusData();
+		System.out.println(getName() + "실행결과 : " + myData.data);
+	}
+}
+
+public class SynchronizedBlock {
+	public static void main(String[] args) {
+		// 공유객체 생성
+		MyData myData = new MyData();
+				
+		// plusThread 1
+		Thread plusThread1 = new PlusThread(myData);
+		plusThread1.setName("plusThread1");
+		plusThread1.start();
+		
+		try { Thread.sleep(1000);} catch (InterruptedException e) {}
+		
+		// plusThread 2
+		Thread plusThread2 = new PlusThread(myData);
+		plusThread2.setName("plusThread2");
+		plusThread2.start();
+	}
+}
