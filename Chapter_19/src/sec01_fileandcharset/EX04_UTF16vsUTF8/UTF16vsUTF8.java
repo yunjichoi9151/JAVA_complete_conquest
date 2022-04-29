7e3ed8ca6c99c91ff96486e4bc35899b820fa439
yunjichoi9151 <cyjcode99@gmail.com>
2022-04-30 00:08:08 +0900

    Add chapter 19 fileandcharset files

---
+++ Chapter_19/src/sec01_fileandcharset/EX04_UTF16vsUTF8/UTF16vsUTF8.java
@@ -0,0 +1,45 @@
+package sec01_fileandcharset.EX04_UTF16vsUTF8;
+import java.io.UnsupportedEncodingException;
+
+public class UTF16vsUTF8 {
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		
+		// UTF-16 vs. UTF-8
+		byte[] b1 = "abc".getBytes("UTF-16"); // 문자열 -> byte[]
+		byte[] b2 = "abc".getBytes("UTF-8");  // 문자열 -> byte[]
+		
+		System.out.println(b1.length);
+		System.out.println(b2.length);
+		
+		for(byte b : b1) 
+			System.out.printf("%02X ", b);
+		System.out.println();
+		
+		for(byte b : b2) 
+			System.out.printf("%02X ", b);
+		System.out.println();
+		
+		System.out.println(new String(b1, "UTF-16"));
+		System.out.println(new String(b2, "UTF-8"));
+		System.out.println();
+		
+		
+		byte[] b3 = "가나다".getBytes("UTF-16"); // 문자열 -> byte[]
+		byte[] b4 = "가나다".getBytes("UTF-8");  // 문자열 -> byte[]
+		
+		System.out.println(b3.length);
+		System.out.println(b4.length);
+		
+		for(byte b : b3) 
+			System.out.printf("%02X ", b);
+		System.out.println();
+		
+		for(byte b : b4) 
+			System.out.printf("%02X ", b);
+		System.out.println();
+		
+		System.out.println(new String(b3, "UTF-16"));
+		System.out.println(new String(b4, "UTF-8")); 
+		System.out.println();
+	}
+}
---
+++ Chapter_19/src/sec01_fileandcharset/EX05_CreateChasetObject/CreateChasetObject.java
@@ -0,0 +1,20 @@
+package sec01_fileandcharset.EX05_CreateChasetObject;
+import java.nio.charset.Charset;
+
+public class CreateChasetObject {
+	public static void main(String[] args) {
+		
+		Charset cs1 = Charset.defaultCharset();
+		Charset cs2 = Charset.forName("MS949");
+		Charset cs3 = Charset.forName("UTF-8");
+		
+		System.out.println(cs1);
+		System.out.println(cs2);
+		System.out.println(cs3);
+		
+		System.out.println();
+		
+		System.out.println(Charset.isSupported("MS949"));
+		System.out.println(Charset.isSupported("UTF-8"));
+	}
+}
